70b970a5277d41829b8e6041f7a01a80a32c2de8
applemagic-01 <thatuserpc@gmail.com>
2025-03-10 20:01:00 +0530

    Update userAuth.js

--- mern-stack/server/middleware/userAuth.js
+++ mern-stack/server/middleware/userAuth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const userAuth = async (req, res, next) => {
     const { token } = req.cookies;
     if (!token) {
-        res.json({ sucsess: false, message: "Not authorized, Login again!" })
+        return res.json({ success: false, message: "Not authorized, Login again!" });
     }
     try {
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
@@ -11,15 +11,14 @@ const userAuth = async (req, res, next) => {
             req.body.userId = tokenDecode.id;
         }
         else {
-            return res.json({ sucsess: false, message: "Not authorized, Login again!" })
+            return res.json({ success: false, message: "Not authorized, Login again!" });
         }
         next();
 
     } catch (error) {
-        res.json({
-            sucsess: false, message: error.message
-        })
-
+        return res.json({
+            success: false, message: error.message
+        });
     }
 }
 
