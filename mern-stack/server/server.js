ad0ea49b917a03d6a4d287221007fd67ae48f3d0
applemagic-01 <thatuserpc@gmail.com>
2025-03-10 20:02:56 +0530

    Update server.js

--- mern-stack/server/server.js
+++ mern-stack/server/server.js
@@ -3,37 +3,36 @@ import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
 
-
 import connectDB from "./config/mongodb.js";
 import authRouter from './routes/authRoutes.js';
 import userRouter from "./routes/userRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 4000
-connectDB();
+const port = process.env.PORT || 4000;
 
-const allowedOrigins = ['https://mern-auth-frontend-0m3x.onrender.com']
-
+const allowedOrigins = ['https://mern-auth-frontend-0m3x.onrender.com'];
 
 app.use(express.json());
 app.use(cookieParser());
+
+// CORS Configuration
 app.use(cors({
   origin: function (origin, callback) {
-    console.log("Origin being checked:", origin);
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
+      console.error("CORS blocked origin:", origin);
       callback(new Error('Not allowed by CORS'));
     }
   },
   credentials: true
 }));
 
-//API endpoints
+connectDB();
+
+// API endpoints
 app.get('/', (req, res) => res.send("API working"));
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
-
-
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
